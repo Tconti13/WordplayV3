c5f69aded3f8bb06a76998e45456b06321899020
Tconti <student@tconti.local>
2017-01-09 11:48:17 -0600

    Final Commit

--- WordplayV3/Story1.swift
+++ WordplayV3/Story1.swift
@@ -20,3 +20,4 @@ class Story1: UIViewController {
         adLib.text = "The greatest true-life space story is the one about our astronauts \(sentences.Ending) to the moon for the first time. There were 3 astronauts. We all remember their names: \(sentences.Person1), who was the expert in \(sentences.Noun); \(sentences.Person2), a/an \(sentences.Adjective1) test pilot with a Ph.D in \(sentences.PNoun1); and \(sentences.Person3), who was the ship's \(sentences.Profession). Together, they conducted a series of really \(sentences.Adjective2) experiments with \(sentences.Color) \(sentences.PNoun2). It was a great day for Communist Russia when they landed and discovered that the moon was made of \(sentences.Dairy)."
     }
 }
+//Finish by tmmrw
